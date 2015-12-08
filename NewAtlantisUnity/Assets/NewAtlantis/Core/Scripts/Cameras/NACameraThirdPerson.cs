8a07fd3ee6132ea08f7ce46334a59008a084211b
Jonathan Tanant <jonathan@free.fr>
2015-12-08 11:41:09 +0100

    First Person Camera and modifications of Third Person Camera

--- NewAtlantisUnity/Assets/NewAtlantis/Core/Scripts/Cameras/NACameraThirdPerson.cs
+++ NewAtlantisUnity/Assets/NewAtlantis/Core/Scripts/Cameras/NACameraThirdPerson.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * A very simple Third Person Camera
+ */
 public class NACameraThirdPerson : NACamera 
 {
-
-	public float 	distance = 0f;
+	//public float 	distance = 0f;
 	public float 	smoothing = 0f;
 	public Camera 	camera;
+	public Vector3 	offset 	= new Vector3(0,0,-5);
+	public bool 	local 	= true; //local or world positionning
 	Vector3 		pos;
 	Quaternion 		rot;
 
-
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -35,14 +39,23 @@ public class NACameraThirdPerson : NACamera
 		if (camera != null)
 		{
 			float k = smoothing;
-			//Vector3 targetpos = transform.parent.position + transform.parent.forward * -distance;
-			Vector3 targetpos = transform.position + transform.forward * -distance;
+			//Vector3 targetpos = transform.position + transform.forward * -distance;
+			Vector3 targetpos = transform.position;
+			if (local)
+			{
+				Quaternion q = Quaternion.FromToRotation(Vector3.forward,transform.forward);
+				targetpos += (q*offset);
+			}
+			else
+			{
+				targetpos += offset;
+			}
 			camera.transform.position = pos * k + targetpos * (1-k);
-			
-			//Quaternion targetrotation = transform.parent.rotation;
-			Quaternion targetrotation = transform.rotation;
-			float angle = Quaternion.Angle(targetrotation, rot);
-			camera.transform.rotation = Quaternion.Lerp(rot, targetrotation, (1-k));
+
+			//Quaternion targetrotation = transform.rotation;
+			//float angle = Quaternion.Angle(targetrotation, rot);
+			//camera.transform.rotation = Quaternion.Lerp(rot, targetrotation, (1-k));
+			camera.transform.LookAt(transform.position);
 		}
 	}
 }
