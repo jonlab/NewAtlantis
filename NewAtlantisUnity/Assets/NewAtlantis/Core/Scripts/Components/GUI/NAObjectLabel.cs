3131867f7372bd7c263c374b4139e32b3a412e79
Jonathan Tanant <jonathan@free.fr>
2015-12-14 11:04:30 +0100

    NAObjectLabel initial

---
+++ NewAtlantisUnity/Assets/NewAtlantis/Core/Scripts/Components/GUI/NAObjectLabel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NAObjectLabel : MonoBehaviour {
+
+
+	public string Title 		= "Your object name.";
+	public string Author 		= "Your name.";
+	public string Date 			= "2015";
+	public string Synopsis 		= "Your object description.";
+	public string Instructions 	= "How to use your object.";
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
