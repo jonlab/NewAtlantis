588298ecdba71707aeb863bd706b9c321994be34
sachaamm <sachaamm@gmail.com>
2016-01-25 16:03:36 -0600

    Final Fix for the Wagner Solar System

---
+++ NewAtlantisUnity/Assets/PG Wagner Solar System/Scripts/Niebelung.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+
+public class Niebelung : MonoBehaviour {
+	public AudioClip mysound;
+	bool on = false;
+    public GameObject myParent;
+    GameObject currentParent;
+
+    void Start () {
+		if (GetComponent<AudioSource>())
+			GetComponent<AudioSource>().Stop ();
+	}
+	void StartEvent ()
+	{
+		GetComponent<Renderer>().material.color = Color.white;
+	
+		on = !on;
+		if (on)
+			
+			GetComponent<AudioSource>().Play ();
+		
+		else
+			
+			GetComponent<AudioSource>().Stop ();
+	}
+	void Update () 
+	{
+		transform.Rotate(new Vector3(Time.deltaTime* -0.35f, Time.deltaTime*3.0f, 0.1f));
+
+	}
+
+    void OnTriggerEnter(Collider e)
+    {
+
+        seekParent(e.transform);
+
+        if (currentParent != myParent) StartEvent();
+
+    }
+
+
+    void seekParent(Transform t)
+    {
+
+        if (t.parent != null)
+        {
+            seekParent(t.parent);
+        }
+        else
+        {
+            currentParent = t.gameObject;
+        }
+
+    }
+}
---
+++ PG Wagner Solar System/Assets/PG Wagner Solar System/Scripts/Notung_new.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public class Notung_new : MonoBehaviour {
+	private AudioSource [] clips;
+	GenaLib mycall = new GenaLib ();
+	//public AudioClip mysound;
+	Vector3 startpos;
+	int choice, abschoice;
+
+    public GameObject myParent;
+    GameObject currentParent;
+
+	// Use this for initialization
+	void Start () 
+	{
+		clips = GetComponents<AudioSource> ();
+		startpos = transform.position;
+		choice = 0;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (!clips [choice].isPlaying) {
+			transform.position = startpos;
+			GetComponent<Rigidbody>().useGravity = false;
+			GetComponent<Renderer>().material.color = Color.yellow;
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+		}
+	}
+
+    void StartEvent()
+    {
+
+        if (NA.isServer() || NA.isStandalone())
+        {
+
+            choice = mycall.Zipf(4);
+            GetComponent<NetworkView>().RPC("syncChoice", RPCMode.All, choice);
+        
+        }
+
+        GetComponent<Renderer>().material.color = Color.white;
+      
+        clips[choice].Play();
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    void OnTriggerEnter(Collider e)
+    {
+      
+        seekParent(e.transform);
+
+        if (currentParent != myParent)
+        {
+            StartEvent();
+          
+        }
+
+      
+
+    }
+
+    void seekParent(Transform t)
+    {
+
+        if(t.parent!= null)
+        {
+            seekParent(t.parent);
+        }
+        else
+        {
+
+            currentParent = t.gameObject;
+        }
+  
+  
+    }
+
+    [RPC]
+    void syncChoice(int _choice)
+    {
+        choice = _choice;
+    }
+
+}
