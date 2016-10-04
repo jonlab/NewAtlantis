5a23321935e37e44e729de820076b81b19299e72
ben <changb3@rpi.edu>
2016-10-04 16:28:17 -0400

    Added an NAForce component.  Lets you set an initial force on an object, and has simple messages to apply a force or a forward vector force.  For clone able objects, if you want them to be "thrown" like the cube spawning tool. (This may be duplicate functionality in another script, if so we can remove this one)

---
+++ NewAtlantisUnity/Assets/NewAtlantis/Users/Scripts/ben_scripts/NAForce.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+
+A script to apply a force to a RigidBody.  You can set an initial force (so something starts moving when instantiated)
+ and also apply a force, to push or animate it in response to an event. 
+*/
+
+public class NAForce : MonoBehaviour {
+	private Rigidbody rigidBody;
+	public float initialForce; 	// initial force along the forward vector when instantiated
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		if (rigidBody != null)
+		{
+			rigidBody.AddForce (initialForce * transform.forward);
+		}
+
+	}
+
+	public void applyForce (Vector3 f)
+	{
+		if (rigidBody != null)
+		{
+			rigidBody.AddForce (f);
+		}
+	}
+
+	public void applyForwardForce (float f)
+	{
+		rigidBody.AddForce (f * transform.forward);
+	}
+
+}
