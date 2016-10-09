f6fb644d2e873218d8576758463b45c1ec68b728
ben <changb3@rpi.edu>
2016-10-09 13:08:23 -0400

    added CrazyTaxi.cs, a crummy behavior for an EarTaxi to randomly drive around

---
+++ NewAtlantisUnity/Assets/NewAtlantis/Users/Scripts/ben_scripts/CrazyTaxi.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrazyTaxi : MonoBehaviour {
+
+	public float speed=1.0f;
+
+	public float turnProbability = 1.0f; 
+
+	public float maxTurnRadius = 90.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!NA.isClient())
+		{
+			float r = Random.value;
+			if (r < turnProbability / 100.0f)
+			{
+				// do random rotation
+
+				float angle = Random.value  * (maxTurnRadius*2) - maxTurnRadius;
+
+				transform.Rotate(0,angle,0);
+			}
+			transform.Translate(Vector3.forward * speed * Time.deltaTime);
+		}
+	}
+}
