08fb9c8780e9ad16f55bb8d453e695615a7cb573
Ben Chang <changb3@rpi.edu>
2017-06-22 18:12:46 +0200

    NeuronCollection.cs

---
+++ NewAtlantisUnity/Assets/Ben/Ben_GameHappens/Ben_MemoryGrids/NeuronCollection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuronCollection : MonoBehaviour {
+	List<GameObject> _nodes;
+
+	void Awake () {
+		_nodes = new List<GameObject> ();
+
+		Neuron[] neurons = GetComponentsInChildren<Neuron> ();
+		foreach (Neuron n in neurons) {
+			_nodes.Add (n.gameObject);
+		}
+
+
+	}
+
+	public List<GameObject> Nodes()
+	{
+		return _nodes;
+	}
+}
