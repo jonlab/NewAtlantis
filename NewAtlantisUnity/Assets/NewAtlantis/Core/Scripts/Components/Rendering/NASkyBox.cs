e202f464f3515f4c61ccdd19f7e45b452e1dae3f
ben <changb3@rpi.edu>
2017-09-28 04:03:29 -0400

    remove shader patching from NASkybox

--- NewAtlantisUnity/Assets/NewAtlantis/Core/Scripts/Components/Rendering/NASkyBox.cs
+++ NewAtlantisUnity/Assets/NewAtlantis/Core/Scripts/Components/Rendering/NASkyBox.cs
@@ -10,14 +10,6 @@ public class NASkyBox : MonoBehaviour
 	{
 		previous = RenderSettings.skybox; //backup previous skybox
 		RenderSettings.skybox = skybox;
-		//patch
-		//skybox.shader = Shader.Find(skybox.shader.name);
-		//patch
-		skybox.shader = Shader.Find("Skybox/6 Sided");
-
-		//if (skybox.shader.name == null)
-		//	skybox.shader = Shader.Find("Skybox/6 Sided");
-	
 	}
 	
 	// Update is called once per frame
@@ -30,7 +22,5 @@ public class NASkyBox : MonoBehaviour
 	{
 		//restore initial skybox
 		RenderSettings.skybox = previous;
-		//patch
-		skybox.shader = Shader.Find(skybox.shader.name);
 	}
 }
