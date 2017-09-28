7d68104fa779ea7e86d15758d0d639b307ef649f
ben <changb3@rpi.edu>
2017-09-28 03:28:08 -0400

    added an alt PS4 mac mapping

--- NewAtlantisUnity/Assets/NewAtlantis/Core/Scripts/Main/NAInput.cs
+++ NewAtlantisUnity/Assets/NewAtlantis/Core/Scripts/Main/NAInput.cs
@@ -29,6 +29,9 @@ public class NAInput
 	static string [] MAPPING_PS4_MAC = new string[13] {"button0","button1","button2","button3",
 		"button5","button4","axis2","axis1","axis4","axis3","axis7","axis8","FullMenu"};
 
+	static string [] MAPPING_PS4_MAC_ALT = new string[13] {"button0","button1","button2","button3",
+		"button5","button4","axis2","axis1","axis4","axis3","axis11","axis12","FullMenu"};
+		
 	static string [] MAPPING_PS4_WIN = new string[13] {"button0","button1","button2","button3",
 		"button5","button4","axis3","axis1","axis7","axis4","axis7","axis8","FullMenu"};
 
