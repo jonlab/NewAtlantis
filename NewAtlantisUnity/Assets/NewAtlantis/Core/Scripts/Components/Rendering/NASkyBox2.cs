fe7a5116e395f5d9d53048d9f6b733d00253a7d8
ben <changb3@rpi.edu>
2017-09-28 03:53:10 -0400

    NASkybox2 script wasn't committed??

---
+++ NewAtlantisUnity/Assets/NewAtlantis/Core/Scripts/Components/Rendering/NASkyBox2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Cubemap skybox
+public class NASkyBox2 : MonoBehaviour 
+{
+	public Material skybox;
+	private Material previous;
+	void Start () 
+	{
+		previous = RenderSettings.skybox; //backup previous skybox
+		RenderSettings.skybox = skybox;
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	void OnDestroy()
+	{
+		RenderSettings.skybox = previous;
+	}
+}
